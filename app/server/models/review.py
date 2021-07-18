91f75cbd5ee246cf63770d04b0e7bf3bf2b2eb66
Santiago Reyes <sreyes@itba.edu.ar>
2021-07-17 22:26:23 -0300

    changed some review field options

--- app/server/models/review.py
+++ app/server/models/review.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel, Field
 
 class ReviewSchema(BaseModel):
     reviewer: str = Field(...)
-    repo_name: str = Field(..., regex="^[a-zA-Z]+/[a-zA-Z_-]+$")
+    repo_name: str = Field(..., regex="^[0-9a-zA-Z]+/[0-9a-zA-Z_-]+$")
     score: float = Field(..., ge=0, le=5)
     comment: str = Field(...)
     # created_at: datetime = Field(...)
 
 class UpdateReviewModel(BaseModel):
-    score: Optional[int] = Field(..., ge=0, le=5)
+    score: Optional[float] = Field(None, ge=0, le=5)
     comment: Optional[str]
 
